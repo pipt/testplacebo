b2bc34d6270a0e6b3333a3a454c7d04c93eb59d3
Phil Taylor <pip@evilgeek.co.uk>
2011-06-26 02:56:01 +0100

    Moved all programs into one file.

--- public/javascripts/programs.js
+++ public/javascripts/programs.js
@@ -11,3 +11,16 @@ window.Programs['rake'] = {
   halt: function() {
   }
 };
+
+window.Programs['echo'] = {
+  init: function() {
+  },
+
+  run: function(args) {
+    OS.programOutput(args.slice(1).join('&nbsp;'));
+    OS.programFinished();
+  },
+
+  halt: function() {
+  }
+};
